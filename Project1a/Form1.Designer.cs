f29a486adcb9e1f83ab0210db61aa460e4daf263
akarpeta <aneta.karpeta@gmail.com>
2020-09-29 10:00:58 +0200

    Kruskal algorithm

---
+++ Project1a/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Project1a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.wczytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wczytajToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1347, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1347, 590);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // wczytajToolStripMenuItem
+            // 
+            this.wczytajToolStripMenuItem.Name = "wczytajToolStripMenuItem";
+            this.wczytajToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.wczytajToolStripMenuItem.Text = "Wczytaj";
+            this.wczytajToolStripMenuItem.Click += new System.EventHandler(this.wczytajToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1347, 614);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem wczytajToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Project1a/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Project1a
+{
+    struct Node
+    {
+        public int ID;
+        public Point position;
+    }
+
+    struct Edge
+    {
+        public int ID;
+        public int node_id_1;
+        public int node_id_2;
+        public int weight;
+    }
+
+    class Graph
+    {
+        public Dictionary<int, Node> nodes = new Dictionary<int, Node>();
+        public Dictionary<int, Edge> edges = new Dictionary<int, Edge>();
+
+        public void Clear()
+        {
+            nodes.Clear();
+            edges.Clear();
+        }
+    }
+
+    class Forest
+    {
+        public List<Graph> forest = new List<Graph>();
+        public List<Edge> edges;
+
+        public void fill(Graph graph)
+        {
+            foreach(var node in graph.nodes.Values)
+            {
+                var local_graph = new Graph();
+                local_graph.nodes.Add(node.ID, node);
+                forest.Add(local_graph);
+            }
+            edges = new List<Edge>(graph.edges.Values);
+        }
+
+        public Graph generate()
+        {
+            List<Edge> S = edges.OrderBy(e => e.weight).ToList();
+            while (S.Count != 0 && forest.Count > 1)
+            {
+                Edge smallest = S[0];
+                S.RemoveAt(0);
+                Graph tree1 = forest.Find(g => g.nodes.ContainsKey(smallest.node_id_1));
+                Graph tree2 = forest.Find(g => g.nodes.ContainsKey(smallest.node_id_2));
+                if (tree1 != tree2)
+                {
+                    forest.Remove(tree2);
+                    tree2.nodes.ToList().ForEach(pair => tree1.nodes.Add(pair.Key, pair.Value));
+                    tree2.edges.ToList().ForEach(pair => tree1.edges.Add(pair.Key, pair.Value));
+                    tree1.edges.Add(smallest.ID, smallest);
+                }
+            }
+            if (forest.Count > 1)
+            {
+                Console.Out.WriteLine("Error..");
+            }
+            return forest[0];
+        }
+    }
+
+   
+
+    public partial class Form1 : Form
+    {
+        Graph graph = new Graph();
+        Graph spanning_tree;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool ReadGraph(string content)
+        {
+            graph.Clear();
+            var lines = content.Split('\n');
+
+            var number_nodes_regex = new Regex(@"WEZLY = (\d+)", RegexOptions.IgnoreCase);
+            var number_edge_regex = new Regex(@"LACZA = (\d+)", RegexOptions.IgnoreCase);
+            var node_regex = new Regex(@"(\d+) (\d+) (\d+)", RegexOptions.IgnoreCase);
+            var edge_regex = new Regex(@"(\d+) (\d+) (\d+) (\d+)", RegexOptions.IgnoreCase);
+
+            int number_nodes = -1;
+            int number_edges = -1;
+            bool read_nodes = true;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("#")) continue;
+                if (line.Trim().Length == 0) continue;
+                if (read_nodes)
+                {
+                    if(number_nodes < 0)
+                    {
+                        var match = number_nodes_regex.Match(line);
+                        if (match.Success)
+                        {
+                            number_nodes = int.Parse(match.Groups[1].Value);
+                            continue;
+                        }
+                        else
+                            return false;
+                    }
+
+                    {
+                        var match = number_edge_regex.Match(line);
+                        if (match.Success)
+                        {
+                            number_edges = int.Parse(match.Groups[1].Value);
+                            read_nodes = false;
+                            continue;
+                        }
+                    }
+
+                    {
+                        var match = node_regex.Match(line);
+                        if (match.Success)
+                        {
+                            Node node = new Node();
+                            node.ID = int.Parse(match.Groups[1].Value);
+                            node.position.X = int.Parse(match.Groups[2].Value);
+                            node.position.Y = int.Parse(match.Groups[3].Value);
+                            graph.nodes.Add(node.ID, node);
+                        }
+                        else
+                            return false;
+                    }
+                }
+                else
+                {
+                    var match = edge_regex.Match(line);
+                    if (match.Success)
+                    {
+                        Edge edge = new Edge();
+                        edge.ID = int.Parse(match.Groups[1].Value);
+                        edge.node_id_1 = int.Parse(match.Groups[2].Value);
+                        edge.node_id_2 = int.Parse(match.Groups[3].Value);
+                        edge.weight = int.Parse(match.Groups[4].Value);
+                        graph.edges.Add(edge.ID, edge);
+                    }
+                    else
+                        return false;
+                }
+            }
+
+            if(number_edges != graph.edges.Count)
+            {
+                return false;
+            }
+
+            if (number_nodes != graph.nodes.Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        Point ScalePoint(Point p)
+        {
+            int location_scale = 5;
+            return new Point(p.X * location_scale, p.Y * location_scale);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            var regular_node_pen = new Pen(Brushes.Blue);
+            Size node_size = new Size(20, 20);
+            Size half_size = new Size(10,10);
+
+            e.Graphics.Clear(Color.White);
+            foreach (var node in graph.nodes.Values)
+            {
+                e.Graphics.DrawRectangle(regular_node_pen, new Rectangle(ScalePoint(node.position), node_size));
+            }
+
+            var regular_edge_pen = new Pen(Brushes.Red);
+            var spanning_edge_pen = new Pen(Brushes.Green);
+
+            foreach (var edge in graph.edges.Values)
+            {
+                Point pos1 = ScalePoint(graph.nodes[edge.node_id_1].position) + half_size;
+                Point pos2 = ScalePoint(graph.nodes[edge.node_id_2].position) + half_size;
+
+                bool from_spanning_tree = spanning_tree.edges.ContainsKey(edge.ID);
+                e.Graphics.DrawLine(from_spanning_tree ? spanning_edge_pen : regular_edge_pen, pos1, pos2);
+
+                e.Graphics.DrawString(edge.weight.ToString(), DefaultFont, Brushes.Black
+                    , new PointF((pos1.X + pos2.X) / 2.0f, (pos1.Y + pos2.Y) / 2.0f));
+            }
+        }
+
+        private void wczytajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+            {
+                String content = sr.ReadToEnd();
+                Console.WriteLine(content);
+                if(!ReadGraph(content))
+                {
+                    Console.WriteLine("Cannot Read File: " + openFileDialog1.FileName);
+                    return;
+                }
+            }
+
+            {
+                Forest kruskal = new Forest();
+                kruskal.fill(graph);
+                spanning_tree = kruskal.generate();
+            }
+
+            panel1.Invalidate();
+        }
+    }
+}
